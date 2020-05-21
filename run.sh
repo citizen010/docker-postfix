67771c29781497e9c1ebdf29d18b3607d621d1c2
Italian Satoshi ™ <57440625+citizen010@users.noreply.github.com>
2020-05-21 12:02:31 +0200

    Change dockerhub bame

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # change your.host.dns to the internet DNS A record and CNAME entry that resolves
 # to your smtp server's docker host. e.g. mail.example.com
-docker run -d --name postfix -p 25:25 --restart always yorkshirekev/postfix your.host.dns
+docker run -d --name postfix -p 25:25 --restart always citizen010/postfix your.host.dns
